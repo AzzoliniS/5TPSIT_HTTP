9c551486357a80e2c28ee8ac058faaff3fff374f
Sveva Azzolini <150719345+AzzoliniS@users.noreply.github.com>
2024-04-03 12:54:44 +0000

    post

--- progetto_express/server.js
+++ progetto_express/server.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const {prodotti} = require("./prodotti")
 
+// Aggiungo alla catena di elaborazioneil middlware body-parser
+// .json() configura body-parser per elaborare i dati contenuti nel body della richiesta HTTP in formato json
+app.use(bodyParser.json());
+// .urlencoded({extended: false}) configura body-parser per elaborare i dati contenuti nel body della richiesta HTTP in formato HTML Form
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+//per qualsiasi richiesta mi fornisce i file contenuti nella cartella public
+app.use("/", express.static(__dirname + "/public"))
 
 app.get('/api/prodotti', (req, res) => {
   res.status(200).json({data: prodotti})
@@ -17,8 +27,14 @@ app.get('/api/prodotti/:id', (req, res)=>{
 })
 
 //aggiungo in coda una nuova persona
-app.post('/api/persone', (req, res)=>{
+app.post('/api/prodotti', (req, res)=>{
   console.log(req.body)
+  if(req.body){
+    prodotti.push(req.body);
+    res.json({data: prodotti})
+  }else{
+    res.status(400).send(" Errore nei dati")
+  }
 })
 
 app.listen(3000)
